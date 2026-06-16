61e51305d9739767823bd0f9ecb635427eb6f4c4
saraqarab <sarahqr20@gmail.com>
2026-06-16 19:06:11 +0500

    minor changes

--- src/yfinance_adapter.py
+++ src/yfinance_adapter.py
@@ -1,11 +1,15 @@
 import pandas as pd
 import yfinance as yf
 
+from src.utils import Utils
+
+utils = Utils
 
 
 class YFinanceAdapter:
     def __init__(self):
         self._cache = {}
+        # self.fall_back_response=""
 
     def get_prices(self, ticker: str, start: str, end: str):
         normalized_ticker = ticker.upper()
@@ -18,14 +22,16 @@ class YFinanceAdapter:
         if raw.empty:
             empty = pd.DataFrame(columns=["date", "ticker", "open", "high", "low", "close", "volume"])
             self._cache[key] = empty
+            # self.fall_back_response = "Data could not be fetched, please try again later"
             return empty.copy()
-
+        # incase yahoo finance returns multi indexed volumes
         if isinstance(raw.columns, pd.MultiIndex):
             raw.columns = raw.columns.get_level_values(0)
-
+        # date index into an actual column
         df = raw.reset_index().rename(columns=str.lower)
         df["ticker"] = normalized_ticker
         result = df[["date", "ticker", "open", "high", "low", "close", "volume"]]
         self._cache[key] = result
-        result.to_excel(f'{ticker}.xlsx', index=False)
-        return result.copy()
+        result.to_excel(f'data/{ticker}.xlsx', index=False)
+        return result.copy() # copy because df are mutable so if I do something like df['start']=0 it'll get corrupted
+
